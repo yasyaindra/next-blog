e9893c111de4c3bfac0c8d18213bf55311b59dc6
yasyaindra <indrayasya96@gmail.com>
2023-05-30 20:32:59 +0700

    link and css modules

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import styles from "../styles/styles.module.css";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="h-screen bg-white">
+    <main className={styles.main}>
+      <nav className="text-white">
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
       <div className="container h-full mx-auto text-black">{children}</div>
-    </div>
+    </main>
   );
 };
 
